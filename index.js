ee0492d968a0b170ef34a40b214a222606171984
silvia <silvia.rodriguez.garcia@hotmail.com>
2019-02-17 19:14:12 -0800

    Add marker as a function and save all markers in hash
    Each position have only one marker. If the status of the position change from free to occupied, we have to remove the marker 'free' and add a marker 'occupied'. The addMarker() function is doing it, using markers variable as hash table. We use "lat+lng" as unique key.

--- index.js
+++ index.js
@@ -1,57 +1,93 @@
 var map;
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 51.523285, lng: -0.1298344},
-    zoom: 20
-  });
-
-  var iconBase = '';
-    var icons = {
-        free: {
-            name: 'Free',
-            icon: 'http://gdurl.com/UHSY'
-        },
-        occupied: {
-            name: 'Occupied',
-            icon: 'http://gdurl.com/1_7E'
-        }
+var iconBase = 'https://drive.google.com/uc?id=';
+var icons = {
+    free: {
+        name: 'Free',
+        icon: iconBase + '1QPAnX97AQF6T-n0o1_gPWCBCIAtrwaud'
+    },
+    occupied: {
+        name: 'Occupied',
+        icon: iconBase + '1bFDrMwPfs0EjfxXbgtTpVCEzeR1wfqyJ'
     }
+}
+
+// keep all the markers of the map
+var markers = {}
+
+function addMarker(parkingSpot) {
+    // if the marker exist, we remove it.
+    if (markers[parkingSpot.lat + '+' +parkingSpot.lng]) {
+        markers[parkingSpot.lat + '+' +parkingSpot.lng].marker.setMap(null)
+    }
+
+    markers[parkingSpot.lat + '+' +parkingSpot.lng] = {
+        type: parkingSpot.type,
+        marker: new google.maps.Marker({
+            position: new google.maps.LatLng(parkingSpot.lat,parkingSpot.lng),
+            icon: {
+                url: icons[parkingSpot.type].icon,
+                scaledSize: new google.maps.Size(30, 30)
+            },
+            map: map
+        })
+    } 
+}
+
+
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 51.523285, lng: -0.1298344},
+        zoom: 20
+    });
 
     var parkings = [
         {
             type: 'free',
-            position: new google.maps.LatLng(51.523518,-0.130158)
+            lat: 51.523518,
+            lng: -0.130158
         },
         {
             type: 'free',
-            position: new google.maps.LatLng(51.523495,-0.130137)
+            lat: 51.523495,
+            lng: -0.130137
         },
         {
             type: 'occupied',
-            position: new google.maps.LatLng(51.523467,-0.130117)
+            lat: 51.523467,
+            lng: -0.130117
         },
         {
             type: 'occupied',
-            position: new google.maps.LatLng(51.523452,-0.130087)
+            lat: 51.523452,
+            lng: -0.130087
         },
         {
             type: 'free',
-            position: new google.maps.LatLng(51.523427,-0.130068)
+            lat: 51.523427,
+            lng: -0.130068
         }
     ]
 
     parkings.forEach(parking => {
-        new google.maps.Marker({
-            position: parking.position,
-            icon: {
-                url: icons[parking.type].icon,
-                scaledSize: new google.maps.Size(30, 30)
-            },
-            map: map
-        });
+        console.log(parking)
+        addMarker(parking);
     })
 }
+
+
+setTimeout(() => {
+    let parking = {
+        type: 'occupied',
+        lat: 51.523427,
+        lng: -0.130068
+    }
+    
+    addMarker(parking);
+
+    console.log(markers)
+}, 10000)
 
 
 
